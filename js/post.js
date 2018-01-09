c16e723727132663c291d7bb59750b597f665c50
Eleyka <eleynekrd@gmail.com>
2018-01-09 10:35:22 -0500

    Desarrollando el post de la Red Social con Firebase

---
+++ js/post.js
@@ -0,0 +1,37 @@
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyDz6Bsp6T9TuR7gYIg5i-tTScsbIFeVyYo',
+  authDomain: 'laboratoria-2a397.firebaseapp.com',
+  databaseURL: 'https://laboratoria-2a397.firebaseio.com',
+  projectId: 'laboratoria-2a397',
+  storageBucket: 'laboratoria-2a397.appspot.com',
+  messagingSenderId: '617399103792'
+};
+firebase.initializeApp(config);
+
+
+/* ----------------------Post---------------------------*/
+// Creaciòn de variables tomando el cuenta el id de cada elemento
+var $txtMensaje = $('#message');
+var $btnEnviar = $('#btnEnviar');
+var $chatUl = $('#chatUl');
+
+// Funciòn del evento click, para almacenar los datos en firebase
+$btnEnviar.on('click', function() {
+  var mensaje = $txtMensaje.val();
+  firebase.database().ref('post').push({
+    message: mensaje
+  });
+});
+
+// Mustra en pantalla los datos a publicar con los datos almacenados en firebase
+firebase.database().ref('post').on('value', function(snapshot) {
+  var html = '';
+  snapshot.forEach(function(e) {
+    var element = e.val();
+
+    var mensaje = element.message;
+    html += '<div>' + mensaje + '</div>';
+  });
+  $($chatUl).append(html);
+});
