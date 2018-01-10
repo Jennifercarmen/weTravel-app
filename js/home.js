86930b236508d84e092c91be30ccdfd0b9d1edeb
Patty8909 <patty90_30@hotmail.com>
2018-01-10 09:22:45 -0500

    añadiendo jquery vista home

--- js/home.js
+++ js/home.js
@@ -9,3 +9,26 @@ $post.on('click', function() {
   window.location.href = 'post.html';
 });
 
+$(document).ready(function() {
+  var validatePost = false;
+
+  $('#message').on('input', function() {
+    if ($(this).val()) {
+      validatePost = true;
+      activeButton();
+    } else {
+      desactiveButton();
+    }
+  });
+
+  function activeButton() {
+    if (validatePost) {
+      $('#btnEnviar').attr('disabled', false);
+    }
+  }
+
+  function desactiveButton() {
+    $('#btnEnviar').attr('disabled', 'disabled');
+  }
+}); 
+
